aba818f383fd5fcec1cefa22727bb220d81cb594
ajrudr <77242143+ajrudr@users.noreply.github.com>
2021-01-31 12:41:19 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,21 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':1.8,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    rectMode(CENTER);
+    fill("purple");
+    rect(pos.x, pos.y, this.width, this.height);
+    pop();
+  }
+};
